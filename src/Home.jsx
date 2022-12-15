2bf016b9fbf29e638af51e053ab6390f7d7ebe25
Zafe123 <scottpilgrim712@gmail.com>
2022-12-15 18:27:33 +0800

    Post title layout

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,29 @@
+
+
 function Home() {
   return (
     <div>
-      I am HOME
+      <div className="d-flex align-items-end flex-column">
+        <div className="col-7 border rounded mt-5 me-5">
+          <div className="col-11 ">
+            <div className="p-3 border rounded mt-5 ms-3">
+              <h3 className>Post Title</h3>
+              <p className="mt-3">Post Body Here</p>
+            </div>
+          </div>
+          <div className="col-11">
+            <div className="p-3 border rounded mt-5 ms-3 mb-5">
+              <h3 className>Post Title</h3>
+              <p className="mt-3">Post Body Here</p>
+            </div>
+          </div>
+          <ul className="ms-3">
+            <li>Post Title</li>
+            <li>Post Title</li>
+            <li>Post Title</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
